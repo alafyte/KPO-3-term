5976a53412e24e8e78254786760e208e2e288d21
alafyte <nroussak0405@gmail.com>
2022-10-01 18:42:59 +0300

    Лабораторная работа №16

---
+++ Lab16/SE_Lab16/SE_Lab16.cpp
@@ -0,0 +1,38 @@
+﻿#include "stdafx.h"
+#include "Error.h"
+#include "Parm.h"
+#include "In.h"
+#include "Log.h"
+#include "Out.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+
+    Log::LOG log = Log::INITLOG;
+    Out::OUT out = Out::INITOUT;
+    try
+    {
+        Parm::PARM parm = Parm::getparm(argc, argv);
+        log = Log::getlog(parm.log);
+        out = Out::getout(parm.out);
+        Log::WriteLine(log, "Тест: ", "без ошибок ", "");
+        Log::WriteLine(log, L"Тест: ", L"без ошибок\n ", L"");
+        Log::WriteLog(log);
+        Log::WriteParm(log, parm);
+        In::IN in = In::getin(parm.in);
+        Log:WriteIn(log, in);
+        Out::WriteIn(out, in);
+        Log::Close(log);
+        Out::Close(out);
+    }
+    catch (Error::ERROR e)
+    {
+        Log::WriteError(log, e);
+        Out::WriteError(out, e);
+    }
+    system("pause");
+    return 0;
+}
+
